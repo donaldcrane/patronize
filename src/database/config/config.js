71c47006ac655b99afb594098d4f5092853fe91f
donaldcrane <donaldagbakoba@gmail.com>
2021-09-18 00:27:44 +0100

    modified config file

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -3,18 +3,24 @@ dotenv.config();
 
 module.exports = {
   development: {
-    username:   process.env.USERN,
-    password:  process.env.PASSWORD,
-    database:  process.env.DATABASE,
-    host:  process.env.HOST,
+    username: process.env.USERN,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
     dialect: process.env.DIALECT,
   },
   test: {
-    url: process.env.TEST_DATABASE_URL,
+    username: process.env.USERN,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
     dialect: process.env.DIALECT,
   },
   production: {
-    url: process.env.PROD_DATABASE_URL,
+    username: process.env.USERN,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
     dialect: process.env.DIALECT,
   },
 };
